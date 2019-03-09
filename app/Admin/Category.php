444f7dcfcb8ae1c4fad6911cfaf9a8305b0c020d
blank <blackhmrr@gmail.com>
2019-03-09 23:23:35 +0700

    update table
    update table

--- app/Admin/Category.php
+++ app/Admin/Category.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    //conntect to table category from mr black
     protected $table = "categories";
+
 
     public function product(){
     	return $this->hasMany(\App\Admin\Product::class);
