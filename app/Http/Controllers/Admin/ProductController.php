ee187a63d428234ea504b8e0d67c9851035c2db9
thachvirak <thachvirak@users.noreply.github.com>
2019-03-10 09:36:45 +0700

    commit

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -10,7 +10,7 @@ use App\Admin\Category;
 use Illuminate\Support\Str;
 class ProductController extends Controller
 {
-    //
+    //controller create product
     public function productList(Request $req){
     	$products = Product::where(['product_status'=> 1])->orderBy('product_name')->get();
     	return view('product.product_list', compact('products'));
