aca530b0a982316438f6e6399a07a36e536d229e
thachvirak <thachvirak@users.noreply.github.com>
2019-03-24 15:57:12 +0700

    create supplier

--- app/Http/Controllers/Admin/SupplierController.php
+++ app/Http/Controllers/Admin/SupplierController.php
@@ -5,24 +5,44 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\User;
 class SupplierController extends Controller
 {
     //
     public function Supplier(){
-
         return view('supplier.supplier');
-
     } 
 
     public function supplierRegister(){
-
         return view('supplier.supplier_register');
-
     } 
+
+    public function createSupplier(Request $req){
+
+        $addUser = new User;
+        $addUser->first_name = $req->first_name;
+        $addUser->last_name  = $req->last_name;
+        $addUser->fullname   = $req->first_name."". $req->last_name;
+        $addUser->email      = $req->email;
+        $addUser->password   = bcrypt($req->password);
+        $addUser->password_text   = $req->password;
+        $addUser->phone      = $req->phone;
+        $addUser->gender         = $req->gender;
+        if($addUser->save()){
+            $status = "success";
+            $message = "<strong> $req->first_name  $req->last_name </strong> Supplier Successfully Registerd";
+        }else{
+            $status = "warning";
+            $message = "Supplier not Registerd";
+        }
+
+        return back()->with(["message"=>$message, "status"=> $status]);
+
+    }
+
     public function supplierLogin(){
 
         return view('supplier.supplier_login');
-
     }
 
     
--- resources/views/supplier/supplier_register.blade.php
+++ resources/views/supplier/supplier_register.blade.php
@@ -4,21 +4,24 @@
 @section('content')
 	@include('partails.home_navbar')
 	<div class="jumbotron">Form Register</div>
+
+	@include('include.message')
 	<div class="container" style="margin: auto; background-color: #6C3483">
 		<div class="row">
 		    <div class="col-sm-9" style="margin: auto; color: white">
 			    <center><h2>Register Form</h2></center>
-			    <form class="form-row">
+			    <form class="form-row" action="{{route('createSupplier')}}" method="POST">
+			    	{{csrf_field()}}
 			    	<div class="col-md-6 mb-3">
 				      <label for="validationServer01">First name*</label>
-				      <input type="text" class="form-control is-valid" id="validationServer01" placeholder="First Name!" required>
+				      <input type="text" class="form-control is-valid" name="fist_name" placeholder="First Name!" required>
 				      <div class="valid-feedback">
 				        Nham!
 				      </div>
 				    </div>
 				    <div class="col-md-6 mb-3">
 				      <label for="validationServer01">Last name*</label>
-				      <input type="text" class="form-control is-valid" id="validationServer01" placeholder="Last Name!" required>
+				      <input type="text" name="last_name" class="form-control is-valid" placeholder="Last Name!" required>
 				      <div class="valid-feedback">
 				        Free!
 				      </div>
@@ -26,21 +29,21 @@
 
 				    <div class="col-md-12 mb-3">
 				      <label for="validationServer01">Your Email*</label>
-				      <input type="text" class="form-control is-valid" id="validationServer01" placeholder="Enter Your Email!" required>
+				      <input type="email" class="form-control is-valid" name="email" placeholder="Enter Your Email!" required>
 				      <div class="valid-feedback">
 				        Example: nhamfree2019@gmail.com!
 				      </div>
 				    </div>
 				    <div class="col-md-12 mb-3">
 				      <label for="validationServer01">Password*</label>
-				      <input type="text" class="form-control is-valid" id="validationServer01" placeholder="Enter Password!" required>
+				      <input type="password" class="form-control is-valid" name="password" placeholder="Enter Password!" required>
 				      <div class="valid-feedback">
 				        Long Than 6 Characters!
 				      </div>
 				    </div>
 				    <div class="col-md-12 mb-3">
 				      <label for="validationServer01">Phone Number*</label>
-				      <input type="text" class="form-control is-valid" id="validationServer01" placeholder="Enter Phone Number!" required>
+				      <input type="text" class="form-control is-valid" name="phone" placeholder="Enter Phone Number!" required>
 				      <div class="valid-feedback">
 				        +855(0)88 2222 999!
 				      </div>
@@ -49,10 +52,10 @@
 				    <div class="col-md-12 mb-3" style="text-align: center;">
 				      <label for="validationServer01">Gender*</label>	      
 				        <label class="col-sm-3">
-				        	<input type="radio" name="remember"> Male
+				        	<input type="radio" name="gender"> Male
 				        </label>
 				        <label>
-				        	<input type="radio" name="remember"> Felmale
+				        	<input type="radio" name="gender"> Felmale
 				        </label>				      
 				    </div>
 
